5d2bb59ff0736864d8d58d7d090f47405750dbe0
LarsTracy <larsatracy@gmail.com>
2023-06-08 19:27:43 -0600

    renamed index.js to service.js, added auth endpoints, disabled execution of user input

---
+++ service.js
@@ -0,0 +1,215 @@
+'use strict'
+
+const { MongoClient } = require('mongodb');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+
+const config = require('./dbConfig.json');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const userColl = client.db('startup').collection('users');
+const cmdColl = client.db('startup').collection('cmdsets');
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(cookieParser())
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+// The top-level express object has a built-in router.
+// apiRouter acts as both a middleware to the top-level
+// mediator, and as a mediator for the api.
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// Define middleware.
+
+// App functionality
+apiRouter.post(`/SubmitCmdSet`, async (req, res) => {
+  // const result = await evaluateCmdSet(req.body.cmd, req.body.stdin);
+  const result = falseEvaluate(req.body.cmd, req.body.stdin);
+  res.status(200).json(result);
+})
+
+apiRouter.post(`/SaveCmdSet`, async (req, res) => {
+  // const result = await evaluateCmdSet(req.body.cmd, req.body.stdin);
+  const result =falseEvaluate(req.body.cmd, req.body.stdin);
+  storeCmdSet(result);
+  res.status(201).json(result);
+})
+
+apiRouter.get(`/BrowseCmdSet`, async (req, res) => {
+  const result = await getAllCmdSets();
+  res.status(200).json(result);
+})
+
+// Auth
+// createAuthorization from the given credentials
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// loginAuthorization from the given credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// getMe for the currently authenticated user
+apiRouter.get('/user/me', async (req, res) => {
+  const authToken = req.cookies['token'];
+  const user = await userColl.findOne({ token: authToken });
+  if (user) {
+    res.send({ email: user.email });
+    return;
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// Return the application's default page if the path is unknown
+// Does omitting the first parameter default to * ?
+app.use((_req, res) => {
+  console.log(`Unknown path: ${_req.path}`);
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Auth helpers
+function getUser(email) {
+  return userColl.findOne({ email: email });
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userColl.insertOne(user);
+
+  return user;
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie('token', authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+// Cmdset processing
+
+// Sadly I don't think I have time to address
+// the security issues with this app in a robust fashion.
+// During development, I'll have endpoints call a bogus
+// evaluation function instead. 
+function falseEvaluate(cmd, stdin) {
+  const stdout = `Given cmd:\n${cmd}\n\nGiven stdin:\n${stdin}`;
+  const stderr = `Due to security concerns, this site does not currently execute user input :)`;
+  return {cmd: cmd, stdin: stdin, stdout: stdout, stderr: stderr}
+}
+
+function isSafeCmd(cmd, stdin) { return true; }
+
+async function evaluateCmdSet(cmd, stdin) {
+  console.log("Evaluating CmdSet");
+  if (not (isSafeCmd(cmd, stdin))) {
+    console.log(`Unsafe cmd: ${cmd}\n${stdin}`);
+    return {cmd: cmd, stdin: stdin, stdout: "", stderr: "That command doesn't look right."};
+  } else {
+    let result = {cmd: cmd, stdin: stdin};
+    const { spawn } = require('child_process');
+    const Readable = require('stream').Readable;
+  
+    // Create child process
+    // I need to split the passed cmd into program and args
+    // This method is poor, all spaces are treated as separating
+    // arguments, and quoting or escaping won't save you.
+    // I also don't support piping like I wanted to,
+    // and may need to refactor to exec instead of spawn so to do.
+    // Executing user input in the shell is a huge security problem.
+    const commandSplit = cmd.split(' ');
+    const child = spawn(commandSplit[0], commandSplit.slice(1));
+  
+    // Create a stream from passed stdin and pipe it to the child
+    const inStream = Readable.from(stdin);
+    inStream.pipe(child.stdin);
+    
+    // Event handler for when the child exits
+    child.on('exit', function (code, signal) {
+      console.log('child process exited with ' +
+                  `code ${code} and signal ${signal}`);
+    });
+    
+    for await (const data of child.stdout) {
+      result.stdout = `${data}`;
+    }
+  
+    for await (const data of child.stderr) {
+      result.stderr = `${data}`;
+    }
+  
+    // When stdout or stderr of the child process have
+    // a data event, write to console and store in result.
+    // child.stdout.on('data', (data) => {
+      // console.log(`child stdout:\n${data}`);
+      // result.stdout = data;
+      // console.log('stdout result', result);
+    // });
+  
+    // child.stderr.on('data', (data) => {
+      // console.error(`child stderr:\n${data}`);
+      // result.stderr = data.toString();
+      // console.log('stderr result', result);
+    // });
+    
+    result.stdout = (typeof result.stdout === 'undefined') ? "" : result.stdout;
+    result.stderr = (typeof result.stderr === 'undefined') ? "" : result.stderr;
+  
+    console.log("Result: ", result);
+    return result;
+  }
+}
+
+
+// Database helpers
+async function storeCmdSet(cmdset) {
+  return await cmdColl.insertOne(cmdset);
+}
+
+async function getAllCmdSets() {
+  result = cmdColl.find();
+  // console.log(await result.toArray());
+  return result.toArray();
+}
+
+// Need to specify cmdsets so I can request remove update specific cmdsets.
+
+
